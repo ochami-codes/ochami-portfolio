280c5062151c9f1d5a2cb188d845b3368375f4d5
ochami-codes <ochamiian29@gmail.com>
2025-10-31 12:15:08 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Header from './components/Header';
-import Hero from './components/Hero';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Header from './assets/Header';
+import Hero from './assets/Hero';
+import About from './assets/About';
+import Projects from './assets/project';
+import Contact from './assets/contacts';
+import Footer from './assets/Footer';
 import './App.css';
 
 export default function App() {
